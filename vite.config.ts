a8e1baabb276e510b33f57611b55212bdb6e173d
sanket1035 <sanketchaudhari1035@gmail.com>
2026-07-30 01:10:44 +0530

    fix(seo): unignore and add public/sitemap.xml and update vercel rewrites for static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   build: {
     chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: 'stats.html',
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+          template: 'treemap',
+        }),
+      ],
+
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
@@ -16,19 +28,21 @@ export default defineConfig({
               id.includes('react-router') ||
               id.includes('scheduler')
             ) {
-              return 'vendor-react';
+              return 'vendor-react'
             }
+
             if (id.includes('framer-motion')) {
-              return 'vendor-framer';
+              return 'vendor-framer'
             }
+
             if (id.includes('lucide-react')) {
-              return 'vendor-lucide';
+              return 'vendor-lucide'
             }
-            return 'vendor-others';
+
+            return 'vendor-others'
           }
         },
       },
     },
   },
 })
-
